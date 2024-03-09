887ddc910be70c0f0a38ddc8d709b71cb58f1f45
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2024-03-09 20:51:25 -0300

    Added basic world gen controls

--- generateWorld.js
+++ generateWorld.js
@@ -3,23 +3,32 @@ import * as THREE from 'three';
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
 
-function generateWorld(scene, worldSize) {
-    const maxCount = worldSize.width * worldSize.height * worldSize.width;
-    const grassBlock = new THREE.InstancedMesh( geometry, material, maxCount );
-    grassBlock.count = 0;
-
-    const matrix = new THREE.Matrix4();
-
-    for (let x = 0; x < worldSize.width; x++) {
-        for (let y = 0; y < worldSize.height; y++) {
-            for (let z = 0; z < worldSize.width; z++) {
-                matrix.setPosition(x, y, z);
-                grassBlock.setMatrixAt(grassBlock.count++, matrix);
-            }
-        }
+class World extends THREE.Group {
+    constructor( size = {width: 16, height: 48} ) {
+        super();
+        this.size = size;
     }
 
-    scene.add(grassBlock);
+    generate() {
+        this.clear();
+
+        const maxCount = this.size.width * this.size.height * this.size.width;
+        const grassBlock = new THREE.InstancedMesh( geometry, material, maxCount );
+        grassBlock.count = 0;
+    
+        const matrix = new THREE.Matrix4();
+    
+        for (let x = 0; x < this.size.width; x++) {
+            for (let y = 0; y < this.size.height; y++) {
+                for (let z = 0; z < this.size.width; z++) {
+                    matrix.setPosition(x, y, z);
+                    grassBlock.setMatrixAt(grassBlock.count++, matrix);
+                }
+            }
+        }
+    
+        this.add(grassBlock);
+    }
 }
 
-export default generateWorld;
+export default World;
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import './style.css';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
 
-import generateWorld from './generateWorld';
+import World from './generateWorld';
 
 // Debug
 const gui = new dat.GUI()
@@ -14,6 +14,9 @@ const textureLoader = new THREE.TextureLoader();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const world = new World();
+scene.add(world);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -33,12 +36,10 @@ scene.add(ambientLight);
 camera.position.z = 80;
 camera.position.y = 20;
 
-const worldSize = {
-  width: 16,
-  height: 48,
-}
+gui.add(world.size, 'width', 1, 32).name('World Width');
+gui.add(world.size, 'height', 1, 80).name('World Height');gui.add(world, 'generate').name('Generate World');
 
-generateWorld(scene, worldSize);
+world.generate();
 
 function animate() {
 	requestAnimationFrame( animate );
